56af9bf85f8cf4c6c709c576ff05e2f1ad121202
L1LCH1LL <108305081+L1LCH1LL@users.noreply.github.com>
2023-02-06 01:22:51 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,22 @@
-﻿// 43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.Write("Введите k1  ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1  ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2  ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2  ");
-var b2 = Convert.ToDouble(Console.ReadLine());
- 
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
 
+Console.Write("Введите колличество чисел :");
+int num = int.Parse(Console.ReadLine()!);
+
+int[] nums = new int[num];
+int count = 0;
+
+for (int i = 0; i < num; i++)
+{
+    nums[i] = int.Parse(Console.ReadLine()!);
+    if (nums[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
 
 
 
